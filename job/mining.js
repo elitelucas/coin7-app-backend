bf4060137ca2634770a7227128877936775a1d76
onecodestar <onecodestar@outlook.com>
2023-04-08 17:45:19 +0900

    finish mining after 24hr

--- job/mining.js
+++ job/mining.js
@@ -1,22 +1,59 @@
-var processes = [];
+
+const User = require('../models/user.js');
+const MiningTransaction = require('../models/MiningTransaction');
+const MlmReferrals = require('../models/MlmReferrals');
+
+var processes = []; //{ user_id, mining_id, amount, time}
 
 exports.initMining = async () => {
     setInterval(() => {
         // console.log('mining running....')
         // console.log(processes)
 
-        processes.map((one) => {
-            /** Blockchain code for mining */
-            one.amount = Math.random() * 1E-4 + Number(one.amount)
+        processes.map(async (item) => {
+            /** TO DO - Blockchain code for mining */
+            item.amount = Math.random() * 1E-4 + Number(item.amount)
+            item.time++;
+
+            if (item.time > 60 * 60 * 24) { //after 24hr from start
+                this.endProcess(item.mining_id);
+
+                let collection = await MiningTransaction.findOne({ _id: item.mining_id });
+                collection.mining_amount = item.amount;
+                collection.end_time = new Date(Date.now())
+                collection.save();
+
+                giveRewardToReferrers(item.user_id, item.amount, 'mining');
+            }
         })
-       
     }, 1000)
 }
 
-exports.addProcess = async (user_id, mining_id) => {
-    processes.push({ user_id, mining_id, amount: 0 })
+const giveRewardToReferrers = async (referee_id, deposit_amount, type) => {
+    const refereeData = await User.findOne({ _id: referee_id });
+    if (!refereeData) return;
+
+    const referrerData = await User.findOne({ _id: refereeData.referrer_id });
+    if (!referrerData) return;
+
+    var reward_amount;
+    reward_amount = deposit_amount * 0.1;
+    await new MlmReferrals({
+        referrer_id: referrerData._id,
+        referee_id: referee_id,
+        deposit_amount: deposit_amount,
+        reward_amount: reward_amount,
+        type: type
+    }).save();
+    return;
 }
 
+//  add mining process 
+exports.addProcess = async (user_id, mining_id) => {
+    processes.push({ user_id, mining_id, amount: 0, time: 0 })
+}
+
+// remove mining process
 exports.endProcess = async (mining_id) => {
     var index = -1;
     for (var i = 0; i < processes.length; i++) {
